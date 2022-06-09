dbf93aec1af16120f32c8c007ee8d18893e589b0
feykke <re.office.25@gmail.com>
2022-06-09 15:48:37 -0300

    ✨ Adding SVG as react components

--- figma.config.js
+++ figma.config.js
@@ -1,8 +1,22 @@
 require('dotenv').config()
 const svgo = require('@figma-export/transform-svg-with-svgo')
+const capitalize = s => {
+    const pageName = s.split('-')
+    const capitalized = pageName.map(str => str.charAt(0).toUpperCase() + str.slice(1))
+    return capitalized.join('')
+}
+
 const fileId = process.env.FILE_ID
+
 const outputters = [ 
-    require('@figma-export/output-components-as-svg')({ output: './' })
+    require('@figma-export/output-components-as-svg')({ output: './svg' }),
+    require('@figma-export/output-components-as-svgr')({
+        getFileExtension: () => ".tsx",
+        getComponentName: ({ componentName, pageName }) =>
+            componentName + capitalize(pageName),
+        getSvgrConfig: () => ({ typescript: true }),
+        output: './src',
+    })
 ]
 
 const solidSVGOConfig = [
